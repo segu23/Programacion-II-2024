cb94aac4facf6c5d7d2b2c6763c7a133c24bd8c4
FranRealSalas <franrealsalas@gmail.com>
2024-04-04 21:50:53 -0300

    punto 10 final

--- TP1/P10.c
+++ TP1/P10.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#define MAX 100
 
 int* explotar(int numeros[], int numero, int bomba, int* contador){
     
@@ -14,22 +15,45 @@ int* explotar(int numeros[], int numero, int bomba, int* contador){
 
     explotar(numeros, N1, bomba, contador);
     explotar(numeros, N2, bomba, contador);
+
+    return numeros;
+}
+
+int* explosion (int n, int b){
+    int *numeros = calloc(MAX,sizeof(int));
+    int contador = 0;
+
+    explotar(numeros, n, b, &contador);
+
+    return numeros;
 }
 
 int main(){
-    int numeros[100];
-    int contador = 0;
-    int numeroExplotar;
-    int bomba;
-    printf("Ingrese el numero a explotar: ");
-    scanf("%d",&numeroExplotar);
-    printf("Ingrese la bomba: ");
-    scanf("%d",&bomba);
+    int numeroExplotar = -1;
+    int bomba = -1;
 
-    explotar(numeros, numeroExplotar, bomba, &contador);
+    while (numeroExplotar<=0){
+        printf("[INPUT] Ingrese el numero a explotar: ");
+        if (scanf("%d", &numeroExplotar)==0){
+            fflush(stdin);
+            printf("[ERROR] El numero ingresado no es un valor permitido\n");
+        }
+    }
+        
+    while (bomba<=0){
+        printf("[INPUT] Ingrese la bomba: ");
+        if (scanf("%d", &bomba)==0){
+            fflush(stdin);
+            printf("[ERROR] El numero ingresado no es un valor permitido\n");
+        }
+    }
 
-    for (int i = 0; i < contador; i++){
-        printf("%i", numeros[i]);
+    int *resultado = explosion(numeroExplotar, bomba);
+
+    for (int i = 0; i < MAX; i++){
+        if (resultado[i]>0){
+            printf("%i ", resultado[i]);
+        }
     }
 
     return 0;
