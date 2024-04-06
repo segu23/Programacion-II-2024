bcacc4919c36b0f7478454b4572918e1195f0694
Segundo Schmidt <segundo.s@autoplants.com.ar>
2024-04-06 15:30:48 -0300

    p7

---
+++ TP1/P7.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+
+// Implementación de recursividad - TP Nro. 1 - Punto 7
+
+// Función recursiva de cola: grafíca la onda a base de valores bivaluados, High ('H') o Low ('L').
+void aplicarOndaDigital(char onda[], int i, char output[], int index)
+{
+    if (onda[i] != '\0')
+    {
+        if (onda[i] != ' ')
+        {
+            if (i != 0 && onda[i - 1] != 'H' && onda[i] == 'H')
+            {
+                output[index] = '|';
+                index++;
+                output[index] = '-';
+                index++;
+            }
+            else if (i != 0 && onda[i - 1] != 'L' && onda[i] == 'L')
+            {
+                output[index] = '|';
+                index++;
+                output[index] = '_';
+                index++;
+            }
+            else if (onda[i] == 'L')
+            {
+                output[index] = '_';
+                index++;
+            }
+            else if (onda[i] == 'H')
+            {
+                output[index] = '-';
+                index++;
+            }
+        }
+        aplicarOndaDigital(onda, ++i, output, index);
+    }
+}
+
+char *ondaDigital(char seniales[])
+{
+    char* output = calloc(MAX, sizeof(char));
+    aplicarOndaDigital(seniales, 0, output, 0);
+
+    return output;
+}
+
+bool validarYCorregirString(char *numero)
+{
+    int longitud = strlen(numero);
+
+    for (int i = 0; i < longitud; i++)
+    {
+        if(numero[i] == 'h') numero[i] = 'H';
+        if(numero[i] == 'l') numero[i] = 'L';
+
+        if (numero[i] != 'H' && numero[i] != 'L')
+        {
+            return false;
+        }
+    }
+
+    return longitud > 0;
+}
+
+int main()
+{
+    char onda[MAX];
+
+    bool valido = false;
+
+    while (!valido)
+    {
+        printf("[INPUT] Ingrese los valores de la onda (H/L): ");
+
+        fgets(onda, sizeof(onda), stdin);
+        onda[strcspn(onda, "\n")] = '\0';
+
+        valido = validarYCorregirString(onda);
+
+        if(!valido){
+            printf("[ERROR] Debe ingresar H o L.\n");
+        }
+    }
+
+    char *output = ondaDigital(onda);
+
+    printf("[OUTPUT] La onda graficada es: %s\n", output);
+
+    system("pause");
+
+    return 0;
+}
