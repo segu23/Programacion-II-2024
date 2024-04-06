8e7f17b2400ff47156c7eeb86f3e1dae3a5b90e6
Segundo Schmidt <segundo.s@autoplants.com.ar>
2024-04-06 13:21:17 -0300

    p2 mejoras

--- TP1/P2.c
+++ TP1/P2.c
@@ -19,27 +19,28 @@ int main(){
     int resultado;
     int lectura;
 
-    printf("ingrese el primer numero(multiplicando):");
+    printf("[INPUT] Ingrese el primer numero(multiplicando): ");
     lectura=scanf("%d", &m);
     while(lectura!=1){
-        printf("ERROR: No se ingreso un numero entero. Vuelva a ingresar el dato.\n");
-        printf("ingrese el primer numero(multiplicando):");
+        printf("[ERROR] No se ingreso un numero entero. Vuelva a ingresar el dato.\n");
+        printf("[INPUT] Ingrese el primer numero(multiplicando): ");
         fflush(stdin);
         lectura=scanf("%d", &m);
     }
-    printf("ingrese el segundo numero(multiplicador):");
+    printf("[INPUT] Ingrese el segundo numero(multiplicador): ");
     lectura=scanf("%d", &n);
     while(lectura!=1){
-        printf("ERROR: No se ingreso un numero entero. Vuelva a ingresar el dato.\n");
-        printf("ingrese el segundo numero(multiplicador):");
+        printf("[ERROR] No se ingreso un numero entero. Vuelva a ingresar el dato.\n");
+        printf("[INPUT] Ingrese el segundo numero(multiplicador): ");
         fflush(stdin);
         lectura=scanf("%d", &n);
     }
 
     resultado=producto(m,n);
 
-    printf("el producto de la multiplicacion es: %d",resultado);
+    printf("[OUTPUT] El producto de la multiplicacion es: %d\n",resultado);
 
+    system("pause");
 
     return 0;
 }
