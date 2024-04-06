61888fc863a56c1bd61facc22649f157863c2b57
FranRealSalas <franrealsalas@gmail.com>
2024-04-06 18:43:33 -0300

    Acomoda P2

--- TP1/P2.c
+++ TP1/P2.c
@@ -2,15 +2,12 @@
 #include <stdio.h>
 
 int producto(int m,int n){
-    if(n==0){
-        
-        return 0;
-        
-    }else{
-
+    if(n==0){  
+        return 0;   
+    }
+    else{
         return m+producto(m,n-1);
     }
-
 }
 
 int main(){
@@ -21,14 +18,17 @@ int main(){
 
     printf("[INPUT] Ingrese el primer numero(multiplicando): ");
     lectura=scanf("%d", &m);
+
     while(lectura!=1){
         printf("[ERROR] No se ingreso un numero entero. Vuelva a ingresar el dato.\n");
         printf("[INPUT] Ingrese el primer numero(multiplicando): ");
         fflush(stdin);
         lectura=scanf("%d", &m);
     }
+
     printf("[INPUT] Ingrese el segundo numero(multiplicador): ");
     lectura=scanf("%d", &n);
+
     while(lectura!=1){
         printf("[ERROR] No se ingreso un numero entero. Vuelva a ingresar el dato.\n");
         printf("[INPUT] Ingrese el segundo numero(multiplicador): ");
@@ -39,7 +39,6 @@ int main(){
     resultado=producto(m,n);
 
     printf("[OUTPUT] El producto de la multiplicacion es: %d\n",resultado);
-
     system("pause");
 
     return 0;
