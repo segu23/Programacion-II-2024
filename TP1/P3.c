3d44f97e110330adef98b0caecf6b75b6203bdb0
Segundo Schmidt <segundo.s@autoplants.com.ar>
2024-04-06 13:22:22 -0300

    mejoras p3

--- TP1/P3.c
+++ TP1/P3.c
@@ -5,6 +5,7 @@ terminoSeridFibonacci (1) => 1
 terminoSeridFibonacci (5) => 8
 */
 #include<stdio.h>
+#include<stdlib.h>
 
 int fibonacci(int numero){
     
@@ -27,17 +28,17 @@ int main(){
 
     int numero;
 
-    printf("Ingrese un numero para el termino de la serie de fibonacci entre 0 y 40: ");
+    printf("[INPUT] Ingrese un numero para el termino de la serie de fibonacci entre 0 y 40: ");
     scanf("%d",&numero);
 
     while ((numero<=0)||(numero>41))
     {
-        printf("Ingrese un numero entre 0 y 40: ");
+        printf("[INPUT] Ingrese un numero entre 0 y 40: ");
         scanf("%d",&numero);
     }
     
     
-    printf("El resultado para el termino %d es %d\n",numero,fibonacci(numero)); 
+    printf("[OUTPUT] El resultado para el termino %d es %d\n",numero,fibonacci(numero)); 
     
     system("pause");
    
