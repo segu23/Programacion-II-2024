cc9d700d573e9108dd35dec77c84e5730018f6c2
segu23 <segundo.s@autoplants.com.ar>
2024-04-15 20:52:45 -0300

    fix p2e

--- TP2/P2e.c
+++ TP2/P2e.c
@@ -8,19 +8,20 @@ E. Que retorne el valor máximo de ambas listas y su posición ordinal.
 #include <stdbool.h>
 
 
-int valor_max1(Lista lista, int * pos){
+int valor_max(Lista lista, int * pos){
     int maximo;
     bool primer_ingreso = false;
     Iterador iter = iterador(lista);
+    int indice = 1;
     
     while (hay_siguiente(iter)) {
         TipoElemento elemento_actual = siguiente(iter);
         if (maximo<elemento_actual->clave || !primer_ingreso){
             maximo=elemento_actual->clave;
-            (*pos)=iter->posicionActual+1;
+            (*pos) = indice;
             primer_ingreso=true;
         }
-        
+        indice++;
     }
 
     return maximo;
