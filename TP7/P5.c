cf182445bf5cf4c4a28db24e3bb24f00fc8f16b8
elsenseixd <valentinlozada1122@gmail.com>
2024-06-16 17:15:48 -0300

    P5

--- TP7/P5.c
+++ TP7/P5.c
@@ -15,30 +15,66 @@
 
 void diferenciaSime(Conjunto conjuntoA,Conjunto conjuntoB){
     Conjunto dif1=cto_diferencia(conjuntoA,conjuntoB);
-    cto_mostrar(dif1);
     Conjunto dif2=cto_diferencia(conjuntoB,conjuntoA);
-    cto_mostrar(dif2);
 
     Conjunto resultado=cto_union(dif1,dif2);
+    printf("\n[OUTPUT] El resultado de la diferencia simetrica entre los dos conjuntos es:\n");
     cto_mostrar(resultado);
-
-
 }
 
 int main(){
-    Conjunto conju=cto_crear();
-    cto_agregar(conju,te_crear(1));
-    cto_agregar(conju,te_crear(2));
-    cto_agregar(conju,te_crear(3));
-    //cto_mostrar(conju);
+    Conjunto conjuntoA=cto_crear();
+    bool seguiragregando=true;
+    int indice=1;
+    int clave=0;
+    char claveChar;
+    while(seguiragregando){
+            
+        printf("[INPUT] Ingrese el elemento %d del primer conjunto ('n' para terminar de cargar el conjunto):",indice);
+        if(scanf("%d",&clave)>0){
+            cto_agregar(conjuntoA,te_crear(clave));
+            indice++;
 
-    Conjunto conju2=cto_crear();
-    cto_agregar(conju2,te_crear(3));
-    cto_agregar(conju2,te_crear(4));
-    cto_agregar(conju2,te_crear(5));
+        }else{
+            if(scanf("%c", &claveChar) > 0 && claveChar == 'n'){
+                seguiragregando = false;
+                printf("[INFO] Terminando ingreso del conjunto.\n");
+            }
+            else{
+                printf("[ERROR] Debe ingresar un valor valido.\n");
+                }
 
-    diferenciaSime(conju,conju2);
+            fflush(stdin);
+
+        }
+    }
+    
+    seguiragregando=true;
+    indice=1;
+
+    Conjunto conjuntoB=cto_crear();
+    while(seguiragregando){
+            
+        printf("[INPUT] Ingrese el elemento %d del segundo conjunto ('n' para terminar de cargar el conjunto):",indice);
+        if(scanf("%d",&clave)>0){
+            cto_agregar(conjuntoB,te_crear(clave));
+            indice++;
+
+        }else{
+            if(scanf("%c", &claveChar) > 0 && claveChar == 'n'){
+                seguiragregando = false;
+                printf("[INFO] Terminando ingreso del conjunto.\n");
+            }
+            else{
+                printf("[ERROR] Debe ingresar un valor valido.\n");
+                }
+
+            fflush(stdin);
+
+        }
+    }
+    
+    diferenciaSime(conjuntoA,conjuntoB);
 
     return 0;
-
 }
